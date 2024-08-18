381cef976c739a50bdf1a3b8a7ca1b90f93d3e06
leokrieger07 <leokriegerrr@gmail.com>
2024-08-18 17:27:33 -0300

    Arrumando

--- exer2.py
+++
@@ -1,7 +0,0 @@
-def imprimir(n):
-    for i in range (1, n+1):
-        print((str(i) + " ")*i )
-
-n= int(input("escreva o valor para rodar o codigo: "))
-
-imprimir(n)
--- exer3.py
+++
@@ -1,8 +0,0 @@
-def imprimir(n):
-    for i in range (1, n + 1):
-        for j in range (1, i + 1):
-            print(j, end = " ")
-        print()
-
-n=int(input("digite um valor para rodar o codigo: "))
-imprimir(n)
